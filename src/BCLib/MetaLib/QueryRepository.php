9d30aa26de86d6ea747d47199ae7a78424011b3f
Ben Florin <benjamin.florin@bc.edu>
2014-06-10 13:20:42 -0400

    Added by-name resource lookup

--- src/BCLib/MetaLib/QueryRepository.php
+++ src/BCLib/MetaLib/QueryRepository.php
@@ -35,16 +35,30 @@ class QueryRepository
         return $this->_sendQuery($qb, $ttl);
     }
 
-    public function sendResourcesQuery($id = null, $ttl = 3600)
+    public function sendResourcesQuery($params, $ttl = 3600)
     {
         $qb = $this->_em->createQueryBuilder();
         $qb->select('Resource')
             ->from('\BCLib\MetaLib\Models\Resource', 'Resource')
             ->orderBy('Resource._name', 'ASC');
-        if (!is_null($id)) {
-            $qb->where($qb->expr()->eq('Resource._number', ':id'))
-                ->setParameter(':id', $id);
+
+        foreach ($params as $key => $value) {
+            switch ($key) {
+                case 'id':
+                    $qb->where($qb->expr()->eq('Resource._number', ':id'))
+                        ->setParameter(':id', $value);
+                    break;
+                case 'first_letter':
+                    $qb->where($qb->expr()->like('Resource._name', ':letter'))
+                        ->setParameter(':letter', "$value%");
+                    break;
+                case 'name':
+                    $qb->where($qb->expr()->like('Resource._name', ':name'))
+                        ->setParameter(':name', "%$value%");
+                    break;
+            }
         }
+
         return $this->_sendQuery($qb, $ttl);
     }
 
--- src/BCLib/MetaLib/Service.php
+++ src/BCLib/MetaLib/Service.php
@@ -34,18 +34,18 @@ class Service
      */
     public function getAllResources($cache_ttl = 3600)
     {
-        return $this->_queries->sendResourcesQuery(null, $cache_ttl);
+        return $this->_queries->sendResourcesQuery([], $cache_ttl);
     }
 
     /**
-     * @param $number    The resource number
-     * @param $cache_ttl Query cache lifetime in seconds
+     * @param $number    string resource number
+     * @param $cache_ttl int cache lifetime in seconds
      *
      * @return \BClib\MetaLib\Models\Resource[]
      */
-    public function getResource($number, $cache_ttl)
+    public function getResource($number, $cache_ttl = 3600)
     {
-        return $this->_queries->sendResourcesQuery($number, $cache_ttl);
+        return $this->_queries->sendResourcesQuery(['id' => $number], $cache_ttl);
     }
 
     /**
@@ -69,5 +69,34 @@ class Service
         return $this->_queries->sendCategoriesQuery($id, $cache_ttl);
     }
 
+    /**
+     * @param string $first_letters
+     * @param int    $cache_ttl
+     *
+     * @return Resource[]s
+     * @throws MetaLibException
+     */
+    public function getResourcesByFirstLetters($first_letters, $cache_ttl = 3600)
+    {
+        if (!is_string($first_letters) || strlen($first_letters) < 1) {
+            throw new MetaLibException("$first_letters is not a valid first letter(s)");
+        }
+        return $this->_queries->sendResourcesQuery(['first_letter' => $first_letters], $cache_ttl);
+    }
+
+    /**
+     * @param string $name
+     * @param int    $cache_ttl
+     *
+     * @return Resource[]
+     * @throws MetaLibException
+     */
+    public function getResourcesByName($name, $cache_ttl = 3600)
+    {
+        if (!is_string($name)) {
+            throw new MetaLibException("$name is not a valid resource name");
+        }
+        return $this->_queries->sendResourcesQuery(['name' => $name], $cache_ttl);
+    }
 
 }
